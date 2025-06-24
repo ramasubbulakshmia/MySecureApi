7714a7018831a18a2439190e69a0d3c24fe977df
ramasubbulakshmia <v-ramasua@microsoft.com>
2025-06-24 12:49:12 +0530

    Add project files.

---
+++ Controllers/FeatureController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MySecureApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public FeatureController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("hello")]
+        public IActionResult SayHello()
+        {
+            
+            return Ok(new { WelcomeNote = "Hi This is my application" });
+        }
+
+        [HttpGet("feature-x")]
+        public IActionResult GetFeatureStatus()
+        {
+            bool isFeatureXEnabled = _configuration.GetValue<bool>("FeatureXEnabled");
+            return Ok(new { FeatureXEnabled = isFeatureXEnabled });
+        }
+
+        [HttpGet("connectionstring")]
+        public IActionResult GetConnectionString()
+        {
+            object? connectionstring = _configuration.GetValue<string>("connectionstring");
+            return Ok(new { Connectionstring = connectionstring?.ToString() });
+        }
+
+        [HttpGet("welcome")]
+        public IActionResult GetWelcomeNote()
+        {
+            object? welcomenote = _configuration.GetValue<string>("welcomenote");
+            return Ok(new { WelcomeNote = welcomenote?.ToString() });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,126 @@
+using Azure.Identity;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+
+var builder = WebApplication.CreateBuilder(args);
+//ConnectAppconfigurationfromLocalhost();
+ConnectAppconfigurationFromAzure();
+
+// Add services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+try
+{
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Run Error: " + ex.Message);
+    throw;
+}
+
+void ConnectAppconfigurationfromLocalhost()
+{
+    //Run from local host
+    // Needed Authorizations when using AzureCliCredential:
+    // 1. Add role assignment " App Configuration Data Reader" on App Configuration to the user account.
+    // 2. Enable system assigned identity in app configuration and add the role assignment "Key Vault Secrets User" in keyvault 
+
+
+    //Needed Authorizations when using DefaultClientCredential
+    // 1.Enable system assigned identity in app configuration and add the role assignment "Key Vault Secrets User" in keyvault 
+    try
+    {
+        AzureCliCredential credential = new AzureCliCredential();
+
+        builder.Configuration.AddAzureAppConfiguration(options =>
+        {
+            //options.Connect(@"Endpoint=https://kalibrate-appconfiguration.azconfig.io;Id=75me;Secret=yWhWjymdgLKCys0eb26TtKHnu401BPXNOirFAG3Sm9rFWaQH79ZsJQQJ99BFACHYHv6Y4SklAAACAZAC3SsQ")
+            options.Connect(new Uri("https://kalibrate-appconfiguration.azconfig.io"), credential)
+                   //.Select("*") // Loads key without any Label
+                   .Select("*", labelFilter: "App1")       // Load keys with label "App2"
+                   .Select("*", labelFilter: LabelFilter.Null) // Load keys without any label
+                   .ConfigureKeyVault(kv =>
+                   {
+                       kv.SetCredential(credential);
+                   });
+        });
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("App Configuration Error: " + ex.Message);
+        throw;
+    }
+}
+
+void ConnectAppconfigurationFromAzure()
+{
+
+    /*After deploying to Azure App Service
+    Needed Authorizations when using user assigned managed identity:
+    1. Create user assigned managed identity 1 and add it under Web App.
+    2. Add role assignments into App Configuration with managed idenitity 1 as "App Configuration Data Reader" - for webapp to appconfiguration
+    3. Create user assigned managed identity 2 and add it under app configuration.
+    4. Add role assignment into Key Vault with managed identity 2 as "Key vault Secrets User"  - for appconfiguration to keyvault
+    5. Add role assignmets into Key Vault with managed identity 1 as "Key vault Secrets User" - for webapp to keyvault
+
+
+    After deploying to Azure App Service 
+    Needed Authorizations when using system assigned managed identity:
+    1. Enable system assigned managed identity(1) in web app
+    2. Add role assignments into App Configuration with webapp's system assigned managed idenitity (1) as "App Configuration Data Reader" - for webapp to appconfiguration
+    3. Enable system assigned managed identity (2) under app configuration.
+    4. Add role assignment into Key Vault with managed identity 2 as "Key vault Secrets User"  - for appconfiguration to keyvault
+    5. Add role assignmets into Key Vault with managed identity 1 as "Key vault Secrets User" - for webapp to keyvault
+    */
+
+    #region Azure Run
+    try
+    {
+
+        // when running in Azure.
+        var credentialOptions_ApptoAppconfig = new DefaultAzureCredentialOptions
+        {
+            ManagedIdentityClientId = "274302c3-a0e5-47ea-af90-739c9311af4a"
+        };
+        var credential_ApptoAppConfig = new DefaultAzureCredential(credentialOptions_ApptoAppconfig);
+
+        // when running in Azure.
+        var credentialOptions_Appconfigtokv = new DefaultAzureCredentialOptions
+        {
+            ManagedIdentityClientId = "c28737b8-a4a3-4a95-a35e-a377a26c1d83"
+        };
+        var credential_Appconfigtokv = new DefaultAzureCredential(credentialOptions_ApptoAppconfig);
+
+
+        builder.Configuration.AddAzureAppConfiguration(options =>
+        {
+            //options.Connect(@"Endpoint=https://kalibrate-appconfiguration.azconfig.io;Id=75me;Secret=yWhWjymdgLKCys0eb26TtKHnu401BPXNOirFAG3Sm9rFWaQH79ZsJQQJ99BFACHYHv6Y4SklAAACAZAC3SsQ")
+            options.Connect(new Uri("https://kalibrate-appconfiguration.azconfig.io"), credential_ApptoAppConfig)
+              .Select("*", labelFilter: "App1")// Optional: apply label filter
+               .Select("*", labelFilter: LabelFilter.Null) // Load keys without any label
+               .ConfigureKeyVault(kv =>
+               {
+                   kv.SetCredential(credential_Appconfigtokv);
+               });
+        });
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("App Configuration Error: " + ex.Message);
+        throw;
+    }
+    #endregion
+}
